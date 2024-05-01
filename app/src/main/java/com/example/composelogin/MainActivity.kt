eaa0b01433af536404c8a87f3ec0ad6a6a80e62a
Mustafa Berk <mustafa.berk@ringcentral.com>
2024-05-01 15:23:43 +0300

    social media icons are implemented.

--- app/src/main/java/com/example/composelogin/MainActivity.kt
+++ app/src/main/java/com/example/composelogin/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -16,8 +18,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -33,6 +33,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -62,92 +63,124 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Login() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(all = 10.dp),
-        verticalArrangement = Arrangement.Center,
-        Alignment.CenterHorizontally
-    ) {
-
-
-        var email by remember { mutableStateOf("") }
-        var password by remember { mutableStateOf("") }
-
-        var passwordVisibility by remember {
-            mutableStateOf(false)
-        }
-
-
-        val icon = if (passwordVisibility) painterResource(id = R.drawable.eye_slash_path) else painterResource(
-            id = R.drawable.eye_path
-        )
-
-
-        Image(
-            imageVector = Icons.Default.AccountCircle,
-            contentDescription = "",
-            modifier = Modifier.size(150.dp)
-        )
-        OutlinedTextField(
+    Box(modifier = Modifier
+        .fillMaxSize()) {
+        Column(
             modifier = Modifier
-                .fillMaxWidth(),
-            shape = RoundedCornerShape(26.dp),
-            value = email,
-            onValueChange = { email = it },
-            label = { Text(text = "Email") },
-            placeholder = {
-                Text(text = "email")
+                .align(Alignment.Center)
+                .padding(all = 10.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+
+
+            var email by remember { mutableStateOf("") }
+            var password by remember { mutableStateOf("") }
+
+            var passwordVisibility by remember {
+                mutableStateOf(false)
             }
-        )
 
-        OutlinedTextField(
-            value = password,
-            shape = RoundedCornerShape(26.dp),
-            modifier = Modifier.fillMaxWidth(),
-            onValueChange = { password = it },
-            label = { Text(text = "Password") },
-            placeholder = {
-                Text(text = "password")
-            },
-            trailingIcon = {
-                IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
-                    Icon(painter = icon, contentDescription = "")
-                }
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password
-            ),
-            visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation()
-        )
 
-        Button(
-            onClick = { }, modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 10.dp)
-        ) {
-            Text(text = "Login")
-        }
-
-        Text(text = "Or", fontSize = 12.sp, modifier = Modifier.padding(top = 10.dp))
-        Box(
-            modifier = Modifier
-                .padding(top = 10.dp)
-                .border(
-                    width = 1.dp,
-                    color = Color.Black,
-                    shape = RoundedCornerShape(20.dp)
-                ),
-            contentAlignment = Alignment.Center
-        ) {
-
-            Text(
-                text = "Register", modifier = Modifier
-                    .padding(all = 5.dp)
+            val icon = if (passwordVisibility) painterResource(id = R.drawable.eye_slash_path) else painterResource(
+                id = R.drawable.eye_path
             )
+
+
+            Image(
+                imageVector = Icons.Default.AccountCircle,
+                contentDescription = "",
+                modifier = Modifier.size(150.dp)
+            )
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(26.dp),
+                value = email,
+                onValueChange = { email = it },
+                label = { Text(text = "Email") },
+                placeholder = {
+                    Text(text = "email")
+                }
+            )
+
+            OutlinedTextField(
+                value = password,
+                shape = RoundedCornerShape(26.dp),
+                modifier = Modifier.fillMaxWidth(),
+                onValueChange = { password = it },
+                label = { Text(text = "Password") },
+                placeholder = {
+                    Text(text = "password")
+                },
+                trailingIcon = {
+                    IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+                        Icon(painter = icon, contentDescription = "")
+                    }
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password
+                ),
+                visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation()
+            )
+
+            Button(
+                onClick = { }, modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp)
+            ) {
+                Text(text = "Login")
+            }
+
+            Text(text = "Or", fontSize = 12.sp, modifier = Modifier.padding(top = 10.dp))
+            Box(
+                modifier = Modifier
+                    .padding(top = 10.dp)
+                    .border(
+                        width = 1.dp,
+                        color = Color.Black,
+                        shape = RoundedCornerShape(20.dp)
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+
+                Text(
+                    text = "Register", modifier = Modifier
+                        .padding(all = 5.dp)
+                )
+            }
+
         }
+        SocialMediaArea(modifier = Modifier.align(Alignment.BottomCenter))
 
     }
+
+
+}
+
+@Composable
+fun SocialMediaArea(modifier:Modifier){
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        MediaIcon(painter = painterResource(id = R.drawable.twitter_svgrepo_com), contentDescription = "twitter")
+        MediaIcon(painter = painterResource(id = R.drawable.facebook_svgrepo_com), contentDescription = "facebook")
+        MediaIcon(painter = painterResource(id = R.drawable.pinterest_svgrepo_com), contentDescription = "pinterest")
+        MediaIcon(painter = painterResource(id = R.drawable.vimeo_svgrepo_com), contentDescription = "vimeo")
+    }
+}
+
+@Composable
+fun MediaIcon(painter: Painter, contentDescription: String) {
+    Image(
+        painter = painter,
+        contentDescription = contentDescription,
+        modifier = Modifier
+            .size(40.dp)
+            .padding(all = 10.dp)
+    )
 }
 
 @Preview(showBackground = true)
