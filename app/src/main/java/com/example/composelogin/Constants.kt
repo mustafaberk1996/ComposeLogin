0922fc5f8e43add9bd1b43fe903e9ff3ff9b2d63
Mustafa Berk <mustafa.berk@ringcentral.com>
2024-05-02 22:52:09 +0300

    coil library added. some flower urls were added to a constant list. random flower show is implemented.

---
+++ app/src/main/java/com/example/composelogin/Constants.kt
@@ -0,0 +1,15 @@
+package com.example.composelogin
+
+object Constants {
+
+
+    val flowerLinks = listOf(
+        "https://images.unsplash.com/photo-1527061011665-3652c757a4d4?q=80&w=2848&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1525310072745-f49212b5ac6d?q=80&w=3098&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+"https://images.unsplash.com/photo-1579664872746-55e2a805d705?q=80&w=3398&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1560256608-43f0b6f7588e?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
+
+
+    )
+}
--- app/src/main/java/com/example/composelogin/MainActivity.kt
+++ app/src/main/java/com/example/composelogin/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -35,8 +36,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
@@ -46,6 +49,7 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.example.composelogin.ui.theme.ComposeLoginTheme
 
 class MainActivity : ComponentActivity() {
@@ -118,10 +122,15 @@ fun Login() {
             )
 
 
-            Image(
-                imageVector = Icons.Default.AccountCircle,
+            AsyncImage(
+                model = Constants.flowerLinks.random(),
                 contentDescription = "",
-                modifier = Modifier.size(150.dp)
+                modifier = Modifier
+                    .size(150.dp)
+                    .fillMaxSize()
+                    .clip(CircleShape)
+                    .border(2.dp, color = Color.Black, shape = CircleShape),
+                contentScale = ContentScale.Crop
             )
             OutlinedTextField(
                 modifier = Modifier
