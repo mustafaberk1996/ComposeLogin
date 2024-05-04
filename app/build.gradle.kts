61f8071a8b3828cd939abb524c2850f135a75e93
mustafaberk1996 <mustafaberk1996@gmail.com>
2024-05-04 22:51:59 +0300

    navigation library is added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 
 dependencies {
 
-    //implementation(libs.material.icons.extended)
+    implementation(libs.navigation.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
